4942d52d605dd223e2bd9800544938933ad4e33e
TSNikita <93394424+TSNikita@users.noreply.github.com>
2021-11-03 22:38:45 +0300

    Add files via upload

---
+++ homework_2.js
@@ -0,0 +1,22 @@
+5 > 4  //true
+"ананас" > "яблоко" //true
+"2" > "12" //fulse
+undefined == null //true
+undefined === null //fulse
+null == "\n0\n"  //true
+null === +"\n0\n" // fulse
+"" + 1 + 0 //"10"
+"" - 1 + 0 // -1
+true + false // 1
+6 / "3" //2
+"2" * "3" // 6
+4 + 5 + "px" //"9px"
+"$" + (4 + 5) //"$45"
+"4" - 2 // 2
+"4px" - 2 //2px
+7 / 0 // Number/ infinity
+"  -9  " + 5 // -4
+"  -9  " - 5 // -14
+null + 1 // 1
+undefined + 1 // NaN
+" \t \n" - 2 //-2
