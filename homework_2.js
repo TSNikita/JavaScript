ccb67279b966910605cc08c6ee0fa0fda47bb684
Nikita Tsipelev <npometall@mail.ru>
2021-11-12 21:58:35 +0300

    ДЗ 1 часть и 2 частично

--- homework_2.js
+++ homework_2.js
@@ -1,246 +1,292 @@
 
 
 
-Дом. задание Лекция №3
+// Дом. задание Лекция №3
 
 
-1
-let a = 0;
+// 1
+// let a = 0;
 
-if (a == 0) {
-    console.log('Верно');
+// if (a == 0) {
+//     console.log('Верно');
 
-} else if (a != 0) {
-console.log('Неверно');
-}
+// } else if {
+// console.log('Неверно');
+// }
 
 
-2
+// 2
 
-let a = 1;
+// let a = 1;
 
-if (a > 0) {
-    console.log('Верно');
+// if (a > 0) {
+//     console.log('Верно');
 
-} else if (a < 0) {
-console.log('Неверно');
+// } else if (a < 0) {
+// console.log('Неверно');
 
-}
+// }
 
 
-3
-let a = -3;
+// 3
+// let a = -3;
 
-if (a < 0) {
-    console.log('Верно');
+// if (a < 0) {
+//     console.log('Верно');
 
-} else if (a > 0) {
-console.log('Неверно');
+// } else if (a > 0) {
+// console.log('Неверно');
 
-}
+// }
 
 
 
-4.
-let a = 1;
+// 4.
+// let a = 1;
 
-if (a >= 0) {
-    console.log('Верно');
+// if (a >= 0) {
+//     console.log('Верно');
 
-} else if (a <= 0) {
-console.log('Неверно');
+// } else if (a <= 0) {
+// console.log('Неверно');
 
-}
+// }
 
 
-5.
-let a = 0;
+// 5.
+// let a = 0;
 
-if (a <=0) {
-    console.log('Верно');
-} else if (a >=0) {
-    console.log('Неверно');
-}
+// if (a <=0) {
+//     console.log('Верно');
+// } else if (a >=0) {
+//     console.log('Неверно');
+// }
 
 
-6.
-let a = -3;
+// 6.
+// let a = -3;
 
-if (a != 0) {
-    console.log('Верно');
-} else if (a == 0) {
-    console.log('Неверно');
-}
+// if (a != 0) {
+//     console.log('Верно');
+// } else if (a == 0) {
+//     console.log('Неверно');
+// }
 
 
-7.
-let a = 'тест';
+// 7.
+// let a = 'тест';
 
-if (a == 'test') {
-    console.log('Верно');
-} else if (a != 'test') {
-    console.log('Неверно');
-}
+// if (a == 'test') {
+//     console.log('Верно');
+// } else if (a != 'test') {
+//     console.log('Неверно');
+// }
 
 
 
-8. 
-let a = '1';
+// 8. 
+// let a = '1';
 
-if (a === '1') {
-    console.log('Верно');
-} else {
-    console.log('Неверно');
-}
+// if (a === '1') {
+//     console.log('Верно');
+// } else {
+//     console.log('Неверно');
+// }
 
 
-работа с логическими переменными
+// работа с логическими переменными
 
-let test = (2 + 2 == 4);
+// let test = (2 + 2 == 4);
 
-if (test == true) {
-    console.log('Верно');
-} else {
-    console.log('Неверно');
-}
+// if (test == true) {
+//     console.log('Верно');
+// } else {
+//     console.log('Неверно');
+// }
 
-let test = 2 + 2 == 4 ? 'Верно' : 'Неверно';
-console.log(test);
+// let test = 2 + 2 == 4 ? 'Верно' : 'Неверно';
+// console.log(test);
 
 
-let test = 1; 
+// let test = 1; 
 
-if (test != true) {
-    console.log('Верно');
-} else {
-    console.log('Неверно');
-}
+// if (test != true) {
+//     console.log('Верно');
+// } else {
+//     console.log('Неверно');
+// }
 
 
 
-let test = 1 ? 'Верно' : 'Неверно';
-console.log(test);
+// let test = 1 ? 'Верно' : 'Неверно';
+// console.log(test);
 
 
-let a = 1; 
-let b = (2*2);
+// let a = 1; 
+// let b = (2*2);
 
-let result = a == b ? 'true' : 'false';
-console.log(result);
+// let result = a == b ? 'true' : 'false';
+// console.log(result);
 
-let a = 'hello';
-let b = 'hi';
+// let a = 'hello';
+// let b = 'hi';
 
-let result = a === b ? 'true' : 'false';
-console.log(result);
+// let result = a === b ? 'true' : 'false';
+// console.log(result);
 
 
-let a = 10;
-let b = 20;
+// let a = 10;
+// let b = 20;
 
-const result = a >= b ? 'true' : 'false';
-console.log(result);
+// const result = a >= b ? 'true' : 'false';
+// console.log(result);
 
 
 
-Работа с И и ИЛИ
+// Работа с И и ИЛИ
 
-let a = 2;
+// let a = 2;
 
-if (a > 0 && a < 5) {
-    console.log('Верно');
-} else {
-    console.log('Неверно');
-}
+// if (a > 0 && a < 5) {
+//     console.log('Верно');
+// } else {
+//     console.log('Неверно');
+// }
 
 
 
-let a = 0;
+// let a = 0;
 
-if (a == 0 || a == 2) {
+// if (a == 0 || a == 2) {
 
-    a += 7;
-} else {
-    a /=10;
-}
-console.log(a);
+//     a += 7;
+// } else {
+//     a /=10;
+// }
+// console.log(a);
 
 
 
-let a = 0;
-let b = 6;
+// let a = 0;
+// let b = 6;
 
-if (a <= 1 && b <= 3) {
+// if (a <= 1 && b <= 3) {
 
-    console.log(a + b);
-} else {
+//     console.log(a + b);
+// } else {
 
-    console.log(a - b);
-}
+//     console.log(a - b);
+// }
 
 
-let a = 5;
-let b = 10;
+// let a = 5;
+// let b = 10;
 
-if (a > 2 && a < 11 || b >= 6 && b < 14) {
-    console.log('Верно');
-} else {
-    console.log('Неверно');
-}
+// if (a > 2 && a < 11 || b >= 6 && b < 14) {
+//     console.log('Верно');
+// } else {
+//     console.log('Неверно');
+// }
 
 
-switch-case
+// switch-case
 
-let num = 4;
+// let num = 3;
 
-switch (num) {
-    case 1:
-        console.log('зима');
-    break;
-    case 2:
-        console.log('весна');
-    break;
-    case 3:
-        console.log('лето');
-    break;
-    case 4:
-        console.log('осень');
-    break;
-} 
+// switch (num) {
+//     case 1:
+//         result = 'зима';
+//     break;
+//     case 2:
+//         result = 'весна';
+//     break;
+//     case 3:
+//         result = 'лето';
+//     break;
+//     case 4:
+//         result = 'осень';
+//     break;
+// } 
 
-Общие задачи
+// console.log(result);
 
-let day = 21;
 
-if (day <= 10) {
-    console.log('1 декада');
 
-} else if (day <=20 ) {
-    console.log('2 декада');
 
-} else if (day <= 31) {
-    console.log('3 декада');
-}
 
-let month = 9;
+// Общие задачи
 
-if (month <=3 ) {
-    console.log('зима');
-} else if (month <=6) {
-    console.log('весна');
-} else if (month <=9) {
-    console.log('лето');
-} else if (month <=12) {
-    console.log('осень');
-}
+// let day = 21;
 
+// if (day <= 10) {
+//     console.log('1 декада');
 
+// } else if (day <=20 ) {
+//     console.log('2 декада');
 
-for (let i = 0; i < 100; i++) {
+// } else if (day <= 31) {
+//     console.log('3 декада');
+// }
+
+// let month = 9;
+
+// if (month <=3 ) {
+//     console.log('зима');
+// } else if (month <=6) {
+//     console.log('весна');
+// } else if (month <=9) {
+//     console.log('лето');
+// } else if (month <=12) {
+//     console.log('осень');
+// }
+
+
+
+// for (let i = 0; i < 100; i++) {
     
-    if(i % 3 == 0) console.log('Fizz');
-    if(i % 5 ==0) console.log('Buzz');
-    if(i % 3 && i %5) console.log('FizzBuzz');
+//     if(i % 3 == 0) console.log('Fizz');
+//     if(i % 5 ==0) console.log('Buzz');
+//     if(i % 3 && i %5) console.log('FizzBuzz');
     
-} 
-console.log(i);
+// } 
+// console.log(i);
+
+
+// дз 2 часть
+
+
+// let num = 1;
+
+// while (num <= 100) {
+//     console.log(num);
+//     num++;
+// }
+
+// while (num <= 33) {
+//     console.log(num);
+//     num++;
+// }
+
+
+// for ( let i = 0; i < 100; i++) {
+//     if (i % 2 == 0) console.log(i);
+// }
+
+
+// С помощью цикла найдите сумму чисел от 1 до 100.
+
+// let result = 0;
+
+// for (i = 1; i <= 100; i++) {
+//     result = result + i;
+// }
+// console.log(result);
+
+
+let day = 1;
+let avto = 20;
+
+for(let i = 1; i <=30; i++) {
+    if (i / 2);
+     console.log(day);
+}
